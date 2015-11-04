52cedb4ef9b89f35b796ae184cfdf8016240b1f3
katemamba <pavan1possible@gmail.com>
2015-11-04 09:02:41 +0530

    first import source

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <?php include('includes/head.php') ?>
+    </head>
+    <body>
+        <div id="home">
+            <h1>Pokemon Challenge Team Rocket</h1>
+            <form action="search.php" method="post">
+                Enter your name: <input type="text" name="name">
+                <input type="submit" title="submit" value="Go!"><br>
+            </form>
+        </div>
+    </body>
+</html>
